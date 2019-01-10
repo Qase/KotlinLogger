fa367bc52e607bdc0b0901fecc5cbf4d060a736b
Vladislav Trnka <vladislav.trnka@quanti.cz>
2019-01-10 14:27:41 +0100

    rewritten UI to constraint layout better button and method names

--- app/src/main/java/quanti/com/kotlinlog3/MainActivity.kt
+++ app/src/main/java/quanti/com/kotlinlog3/MainActivity.kt
@@ -32,19 +32,21 @@ class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
         Log.addLogger(FileLogger(applicationContext))
 
         Log.useUncheckedErrorHandler()
-        Log.addLogger(AndroidLogger)
+        Log.addLogger(AndroidLogger())
 
         Fabric.with(this, Crashlytics())
-        Log.addLogger(CrashlyticsLogger)
+        Log.addLogger(CrashlyticsLogger())
 
-
-        (findViewById<RadioGroup>(R.id.radio_group)).setOnCheckedChangeListener(this)
-
+        (findViewById<RadioGroup>(R.id.radioGroup)).setOnCheckedChangeListener(this)
 
     }
 
+    override fun onCheckedChanged(group: RadioGroup, @IdRes checkedId: Int) {
+        val text = group.findViewById<RadioButton>(checkedId).text
+        checked = Integer.parseInt(text[text.length - 1] + "")
+    }
 
-    fun button_clicked(view: View) {
+    fun hitme_clicked(view: View) {
         when (checked) {
             LogLevel.DEBUG -> Log.d("laisdjlakdj")
             LogLevel.ERROR -> Log.e("laisdjlakdj")
@@ -54,18 +56,7 @@ class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
         }
     }
 
-    fun button_clicked_throw(view: View) {
-        Log.e("Something bad happened", ArrayIndexOutOfBoundsException("Message in exception"))
-    }
-
-    override fun onCheckedChanged(group: RadioGroup, @IdRes checkedId: Int) {
-
-        val text = group.findViewById<RadioButton>(checkedId).text
-        checked = Integer.parseInt(text[text.length - 1] + "")
-    }
-
-    fun burst_clicked(view: View) {
-
+    fun testone_clicked(view: View) {
         val end = 50
 
         Flowable
@@ -74,7 +65,7 @@ class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
                 .subscribe { Log.i(it) }
     }
 
-    fun burst_thread_clicked(view: View) {
+    fun testtwo_clicked(view: View) {
 
         val threads = 5
         val end = 1000
@@ -88,9 +79,76 @@ class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
 
     }
 
-    fun cz_clicked(view: View) {
+    fun testthree_clicked(view: View) {
+        Flowable.range(0, 10000)
+                .subscribe {
+                    if (it % 1000 == 0) {
+                        Log.e("Something bad happened $it", ArrayIndexOutOfBoundsException("Message in exception $it"))
+                    }
+                    Log.i(it.toString())
+                }
+    }
+
+    fun testfour_clicked(view: View) {
+        Flowable.range(0, 10000)
+                .subscribeOn(Schedulers.newThread())
+                .subscribe {
+                    if (it % 1000 == 0) {
+                        Log.e("Something bad happened $it", ArrayIndexOutOfBoundsException("Message in exception $it"))
+                    }
+                    Log.i(it.toString())
+                }
+
+        Flowable.range(0, 10000)
+                .subscribeOn(Schedulers.newThread())
+                .subscribe {
+                    if (it % 1000 == 0) {
+                        Log.e("Something bad happened $it", ArrayIndexOutOfBoundsException("Message in exception $it"))
+                    }
+                    Log.i(it.toString())
+                }
+
+        Flowable.range(0, 10000)
+                .subscribeOn(Schedulers.newThread())
+                .subscribe {
+                    if (it % 1000 == 0) {
+                        Log.e("Something bad happened $it", ArrayIndexOutOfBoundsException("Message in exception $it"))
+                    }
+                    Log.i(it.toString())
+                }
+
+    }
+
+    fun throwException_clicked(view: View) {
+        Log.e("Something bad happened", ArrayIndexOutOfBoundsException("Message in exception"))
+    }
+
+    fun throwUncheckedException_clicked(view: View) {
+        throw ArrayIndexOutOfBoundsException("unchecked exception")
+    }
+
+    fun logMetadata_clicked(view: View) {
+        Log.logMetadata(applicationContext)
+    }
+
+
+    fun send_clicked(view: View) {
 //        SendLogDialogFragment.newInstance("kidal5@centrum.cz", deleteLogs = true).show(supportFragmentManager, "OMG")
     }
+
+    fun deleteLogs_clicked(view: View) {
+        FileLogger.deleteAllLogs(applicationContext)
+        Toast.makeText(this, "Logs deleted", Toast.LENGTH_SHORT).show()
+    }
+
+
+
+
+
+
+
+
+
 
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -114,62 +172,6 @@ class MainActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
 //                    .create()
 //                    .show()
 //        }
-    }
-
-    fun test_1(view: View) {
-        FileLogger.deleteAllLogs(applicationContext)
-        Toast.makeText(this, "Logs deleted", Toast.LENGTH_SHORT).show()
-    }
-
-
-    fun test_2(view: View) {
-        Flowable.range(0, 10000)
-                .subscribe {
-                    if (it % 1000 == 0) {
-                        Log.e("Something bad happened $it", ArrayIndexOutOfBoundsException("Message in exception $it"))
-                    }
-                    Log.i(it.toString())
-                }
-    }
-
-    fun test_3(view: View) {
-        //first log to sync logger
-
-        Flowable.range(0, 10000)
-                .subscribeOn(Schedulers.newThread())
-                .subscribe {
-                    if (it % 1000 == 0) {
-                        Log.e("Something bad happened $it", ArrayIndexOutOfBoundsException("Message in exception $it"))
-                    }
-                    Log.i(it.toString())
-                }
-
-        Flowable.range(0, 10000)
-                .subscribeOn(Schedulers.newThread())
-                .subscribe {
-                    if (it % 1000 == 0) {
-                        Log.e("Something bad happened $it", ArrayIndexOutOfBoundsException("Message in exception $it"))
-                    }
-                    Log.i(it.toString())
-                }
-
-        Flowable.range(0, 10000)
-                .subscribeOn(Schedulers.newThread())
-                .subscribe {
-                    if (it % 1000 == 0) {
-                        Log.e("Something bad happened $it", ArrayIndexOutOfBoundsException("Message in exception $it"))
-                    }
-                    Log.i(it.toString())
-                }
-
-    }
-
-    fun test_4(view: View) {
-        throw ArrayIndexOutOfBoundsException("unchecked exception")
-    }
-
-    fun test_5(view: View) {
-        Log.logMetadata(applicationContext)
     }
 
 }
